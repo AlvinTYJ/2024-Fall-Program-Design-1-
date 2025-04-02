23b251d22b78e8c0b25b74c3174d9883d9bf363c
Alvin <113426752+AlvinTYJ@users.noreply.github.com>
2025-04-03 00:14:08 +0800

    Add files via upload

---
+++ HW1/pA/pA.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    printf("Welcome to NCKU Program Design I %d\n", a);
+}
---
+++ HW1/pB/pB.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    scanf("NCKU CSIE%dPrograming Design I.",&x);
+    printf("%d\n",x);
+}
